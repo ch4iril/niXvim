ea8a3a847d94ea2da50be49e786620abd949a53c
ch4iril <chairil.iril@gmail.com>
2024-10-20 12:48:51 +0700

    add colorscheme variant

---
+++ config/colorscheme.nix
@@ -0,0 +1,14 @@
+{
+
+  colorschemes.ayu.enable = true;
+  colorschemes.catppuccin.enable = true;
+  colorschemes.dracula.enable = true;
+  colorschemes.dracula-nvim.enable = true;
+  colorschemes.gruvbox.enable = true;
+  colorschemes.kanagawa.enable = true;
+  colorschemes.one.enable = true;
+  colorschemes.onedark.enable = true;
+  colorschemes.tokyonight.enable = true;
+  colorschemes.vscode.enable = true;
+
+}
---
+++ config/lsp.nix
@@ -0,0 +1,6 @@
+{
+  plugins.lsp.enable = true;
+  plugins.lsp.servers.nixd.enable = true;
+  plugins.lsp.servers.nixd.autostart = true;
+
+}
